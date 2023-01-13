731ceae137d0bc19568a3ff85bd0f9fe9509b478
IreneLeon23 <118926696+IreneLeon23@users.noreply.github.com>
2023-01-13 13:28:48 -0600

    Add files via upload

---
+++ contador/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import BarraSuperior from './components/BarraSuperior';
+import Contador from './components/Contador';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <BarraSuperior/>
+      <Contador/>
+    </div>
+  );
+}
+
+export default App;
